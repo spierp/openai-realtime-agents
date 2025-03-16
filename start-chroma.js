2fa245af91af2bff30ad2727527248c46346edd6
Peter Spier <spierp@gmail.com>
2025-03-16 17:54:34 +0000

    chroma running

--- start-chroma.js
+++ start-chroma.js
@@ -1,24 +1,21 @@
 // Simple script to test ChromaDB client connection
-const { ChromaClient } = require('chromadb');
+const { ChromaClient } = require("chromadb");
+const client = new ChromaClient({ path: "http://localhost:8000" });
 
 console.log("Testing connection to ChromaDB server...");
-
-// Create a client that connects to the server
-const client = new ChromaClient({
-  path: "http://localhost:8000" 
-});
 
 // Test connection with heartbeat
 console.log("Attempting to connect to ChromaDB server...");
 setTimeout(() => {
   // Try listing collections directly instead of using heartbeat
-  client.listCollections()
-    .then(collections => {
+  client
+    .listCollections()
+    .then((collections) => {
       console.log("Connected to ChromaDB server successfully!");
       console.log("Available collections:", collections);
     })
-    .catch(err => {
-      console.error('Error connecting to ChromaDB server:', err);
+    .catch((err) => {
+      console.error("Error connecting to ChromaDB server:", err);
       console.log("Please start the ChromaDB server first with:");
       console.log("python start-chroma-server.py");
     });
